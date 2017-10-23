293d317a738dd472ac5c87ea4def2e41ce7bd7e9
Carlos Ungil <cungil@common-lisp.net>
2017-10-23 15:32:16 +0200

    fix example

--- README.md
+++ README.md
@@ -37,13 +37,13 @@ plot_frontier()
 ![frontier2](examples/frontier2.png)
 
 ````julia
-m=markowitz(E, V, names=assets, #  asset bounds by class: eq -10/30, fi 0/20, alt 0/10
+m=markowitz(E, V, names=assets, # asset bounds by class: stocks -10/30, bonds 0/20, alt. 0/10
             lower = -0.1 * (class .== :EQ),
 	    upper = 0.3 * (class .== :EQ) + 0.2 * (class .== :FI) + 0.1 * (class .== :ALT))
 unit_sum(m)
 add_constraint(m, 1 * (class .== :EQ), '>', 0.3) # net equity exposure between 30% and 60%
 add_constraint(m, 1 * (class .== :EQ), '<', 0.6)
-add_constraint(m, [1 1 0 0 0 0 0 0 0 0 0 0 0 0], '=',0.25) # US govt + Investment Grade = 25%
+add_constraint(m, [1 1 0 0 0 0 0 0 0 0 0 0 0 0], '=', 0.25) # US govt + Investment Grade = 25%
 f=frontier(m)
 plot_frontier()
 ````
--- examples/frontier.jl
+++ examples/frontier.jl
@@ -112,17 +112,17 @@ optimal(f,4) # volatility, return and weights for the optimal portofolio with re
 
 
 m=markowitz(E, V, names=assets, lower=0.05, upper=0.15) # min 5%, max 15% per position
-unit_sum(m) # total weight = 100%
+unit_sum(m)
 f=frontier(m)
 plot_frontier()
 
 
-m=markowitz(E, V, names=assets, #  asset bounds by class: eq -10/30, fi 0/20, alt 0/10
+m=markowitz(E, V, names=assets, # asset bounds by class: stocks -10/30, bonds 0/20, alt. 0/10
             lower = -0.1 * (class .== :EQ),
             upper = 0.3 * (class .== :EQ) + 0.2 * (class .== :FI) + 0.1 * (class .== :ALT))
-unit_sum(m) # total weight = 100%
+unit_sum(m)
 add_constraint(m, 1 * (class .== :EQ), '>', 0.3) # net equity exposure between 30% and 60%
 add_constraint(m, 1 * (class .== :EQ), '<', 0.6)
-add_constraint(m, [1 1 0 0 0 0 0 0 0 0 0 0 0 0], '=',0.25) # US govt + Investment Grade = 25%
+add_constraint(m, [1 1 0 0 0 0 0 0 0 0 0 0 0 0], '=', 0.25) # US govt + Investment Grade = 25%
 f=frontier(m)
 plot_frontier()
